2883b2e22232f80304e50d2025f65bba0e16220c
Frank Gimeno <frankgimeno3@gmail.com>
2023-05-31 01:22:23 +0200

    mejoras de estilos implementadas

--- components/Home/MainHeader.tsx
+++ components/Home/MainHeader.tsx
@@ -2,23 +2,25 @@ import React from 'react';
 
 const MainHeader = () => {
   return (
-      < >
-        <video
-          className="w-full h-auto object-cover"
-          src="/video/perfiles.mp4"
-          autoPlay
-          loop
-          muted
-        ></video>
-        <div className="absolute inset-0 flex flex-col justify-center items-center">
-          <h1 className="text-4xl md:text-6xl text-white font-bold mb-6">
-            Tecnobolsa
+    <div className="relative w-full h-screen">
+      <video
+        className="absolute top-0 left-0 object-cover w-screen h-screen"
+        src="/video/perfiles.mp4"
+        autoPlay
+        loop
+        muted
+      ></video>
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center">
+        <div className="bg-gray-800 bg-opacity-60 backdrop-filter backdrop-blur-lg w-full pt-10 pb-10">
+          <h1 className="text-4xl md:text-6xl text-gray-100 font-bold">
+            TECNOBOLSA
           </h1>
-          <p className="text-xl text-white text-center">
+          <p className="text-xl text-white max-w-[45vw] m-auto pt-10 font-light">
             Conectamos empresas con personal técnico especializado en los 21 países de habla hispana. Únase a nosotros para encontrar talento o empleo desde hoy mismo.
           </p>
         </div>
-      </>
+      </div>
+    </div>
   );
 };
 
