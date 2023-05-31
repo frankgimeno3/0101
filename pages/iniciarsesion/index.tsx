c060a909350aa1a6088f2333f1f34438c9812a55
Frank Gimeno <frankgimeno3@gmail.com>
2023-05-31 02:55:09 +0200

    agrego registro e inicio de sesión

--- pages/iniciarsesion/index.tsx
+++ pages/iniciarsesion/index.tsx
@@ -1,12 +1,66 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import { useState, ChangeEvent, FormEvent } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function IniciarSesion() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-    <h1>Esta es la vista de iniciar sesion</h1>
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Aquí puedes implementar la lógica de inicio de sesión seguro
+  };
+
+  return (
+    <div className="flex min-h-screen flex-col items-center  pt-24 text-center">
+      <h1 className="text-4xl mb-10">Iniciar sesión</h1>
+
+      <form onSubmit={handleLogin} >
+        <div className="mb-4">
+          <label htmlFor="email" className="block mb-2 text-lg font-medium">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            className="w-full px-4 py-2 border rounded-md"
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label htmlFor="password" className="block mb-2 text-lg font-medium">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            className="w-full px-4 py-2 border rounded-md"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full py-2 px-4 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+        >
+          Iniciar sesión
+        </button>
+      </form>
     </div>
-  )
+  );
 }
--- pages/registro/index.tsx
+++ pages/registro/index.tsx
@@ -1,12 +1,65 @@
-import { Inter } from 'next/font/google'
+import { useState } from 'react';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Registro() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [accountType, setAccountType] = useState('');
+  const [country, setCountry] = useState('');
 
-    <h1>Esta es la vista de Registro</h1>
+  const handleRegister = () => {
+    // Aquí puedes implementar la lógica de registro seguro
+    console.log('Registro seguro:', email, password, accountType, country);
+  };
+
+  return (
+    <div className="flex min-h-screen flex-col items-center  p-24">
+      <h1 className="text-4xl">Crear cuenta</h1>
+
+      <input
+        type="email"
+        placeholder=" Introducir Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="p-2 m-2 border border-gray-300 rounded-md"
+      />
+
+      <input
+        type="password"
+        placeholder="Introducir Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="p-2 m-2 border border-gray-300 rounded-md"
+      />
+
+      <select
+        value={accountType}
+        onChange={(e) => setAccountType(e.target.value)}
+        className="p-2 m-2 border border-gray-300 rounded-md"
+      >
+        <option value="">Seleccione tipo de cuenta</option>
+        <option value="empresa">Empresa</option>
+        <option value="profesional">Profesional</option>
+      </select>
+
+      <select
+        value={country}
+        onChange={(e) => setCountry(e.target.value)}
+        className="p-2 m-2 border border-gray-300 rounded-md"
+      >
+        <option value="">Seleccione país</option>
+        <option value="país1">País 1</option>
+        <option value="país2">País 2</option>
+      </select>
+
+      <button
+        onClick={handleRegister}
+        className="px-4 py-2 bg-blue-500 text-white rounded-md"
+      >
+        Registrar
+      </button>
     </div>
-  )
+  );
 }
